ba6239b678766e7200cab38ce34bfd53bc3aa78a
Tyler Rivera <riverat2@email.chop.edu>
2016-09-26 16:18:49 -0400

    Release 1.0.1

--- ehb_client/__init__.py
+++ ehb_client/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
-    'major': 0,
-    'minor': 6,
-    'micro': 0,
-    'releaselevel': 'beta',
-    'serial': 8
+    'major': 1,
+    'minor': 0,
+    'micro': 1,
+    'releaselevel': 'final',
+    'serial': 9
 }
 
 def get_version(short=False):
